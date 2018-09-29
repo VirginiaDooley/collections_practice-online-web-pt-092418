d5017441c59793b6b68668604c62e308633c8fc3
Virginia Dooley <vcdooley@gmail.com>
2018-09-29 14:22:08 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -55,6 +55,6 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect { |el, index| el + index }
+  array.each_with_index.collect { |element, index| element + ("s") if index < 2 }
 end
 end
