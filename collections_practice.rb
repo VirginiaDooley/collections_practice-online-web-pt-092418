65ec6b371ff46ef5b1ea04f2aa86207dee95a21c
Virginia Dooley <vcdooley@gmail.com>
2018-09-29 14:16:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,8 +31,8 @@ def swap_elements(array)
   array
 end
 
-def swap_elements_from_to(array, index, index2)
-  # array[index], array[index2] = array[index2], array[index]
+def swap_elements_from_to(array, index, destination_index)
+  # array[index], array[destination_index = array[destination_index], array[index]
   # array
 end
 
