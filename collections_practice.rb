8866bc7b5f886dc3e573b269a9429a8703abc05d
Virginia Dooley <vcdooley@gmail.com>
2018-09-29 14:56:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,10 +56,10 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |word,index|
-        if index = 1 
-          word = word + "s"
-        else
-          word = word
-        end
+    if index = 1 
+      word = word + "s"
+    else
+      word = word
+    end
   end
 end
